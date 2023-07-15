0554e4d8f2f2a000534388980902dff9e3fbabc5
mayowasamuel <87167237+Mayowasamuel51@users.noreply.github.com>
2023-07-15 15:04:34 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+// // console.log('working')
+
+// // let
+// let name = "sara";
+// let num_one = 1;
+
+// name = "mayor"
+
+// console.log(name)
+// console.log(num_one)
+
+// let sercetkey = 's,dofpnivndavnskdcvndzojvbnkdzob';
+
+// sercetkey = 'jodsncuaodnvsfnvs'
+// //const
+// const myName = "mayowa james"
+// // myName = "james i"
+// console.log(myName)
+
+// let apiKey = 'disancuadbvcubdnadbvzbcvcz bhv scb vcdcacsachs';
+// apiKey = 'sara'
+// console.log(apiKey)
+
+// // use const
+// const glass = 'main glass hill'
+// glass = 'hill'
+// console.log(glass)
+
+
+
+
+
+//// How to store info/data in js
+// Array  []  ===   string, number , Boolean
+
+// 0 
+const names = ['mayor', 'lola', 'james', 'bukky', 'shola']
+const otherValues = ['name value', false, true,  1,  400, 'main ']
+console.log(names)
+
+const something = ['disvndfs', 'sdinsafca','safda', 2123]
+// console.log(otherValues)
+// names.pop() //remove the last elements from an array ............
+
+// names.pop()
+
+// names.push('new name added')
+// names.unshift('frist item')
+// names.shift()
+
+
+console.log(names)
